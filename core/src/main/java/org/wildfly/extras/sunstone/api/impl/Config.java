e1a8d81f775d880b26e9b9518085adc74c9ff4fe
Richard Janík <rjanik@redhat.com>
2016-09-02 13:58:19 +0200

    Removed superfluous comments from Config, EC2 related

--- core/src/main/java/org/wildfly/extras/sunstone/api/impl/Config.java
+++ core/src/main/java/org/wildfly/extras/sunstone/api/impl/Config.java
@@ -72,31 +72,11 @@ public final class Config {
          * EC2 cloud provider related keys. When changing, don't forget to change {@code ec2-README.md}.
          */
         public static final class EC2 {
-            /**
-             * The AMI query which is used for searching the AMI. Setting this option should not be necessary if you don't intend to search for AMIs (e.g. if you have a specific AMI in mind)
-             */
             public static final String AMI_QUERY = "ec2.ami.query";
-            /**
-             * The region for the cloud provider. Usually us-east-1.
-             * @See org.jclouds.aws.domain.Region
-             */
             public static final String REGION = "ec2.region";
-            /**
-             * If set to true, a logging module for EC2 cloud provider will be added.
-             * This logs the ec2 operations the cloud provider does. E.g. how it lists the AMIs available, how it starts an instance, etc. Lots of output, beware!
-             */
             public static final String LOG_EC2_OPERATIONS = "ec2.logEC2Operations";
-            /**
-             * The endpoint to contact. Setting it isn't mandatory and the default endpoint should be the default endpoint for the region.
-             */
             public static final String ENDPOINT = "ec2.endpoint";
-            /**
-             * An access key ID of an IAM user. See EC2 console for more information on this.
-             */
             public static final String ACCESS_KEY_ID = "ec2.accessKeyID";
-            /**
-             * The secret access key of an IAM user. This key needs to be bound to the access key ID. See EC2 console for more information.
-             */
             public static final String SECRET_ACCESS_KEY = "ec2.secretAccessKey";
         }
 
@@ -195,83 +175,20 @@ public final class Config {
          * Configuration keys for EC2 nodes. When changing, don't forget to change {@code ec2-README.md}.
          */
         public static final class EC2 {
-            /**
-             * <a href="https://aws.amazon.com/ec2/instance-types/">Instance type</a> for the node. This defines the
-             * computing/networking/... capabilities of the node.
-             *
-             * @See org.jclouds.ec2.domain.InstanceType
-             */
             public static final String INSTANCE_TYPE = "ec2.instance.type";
             public static final String IMAGE = "ec2.image";
-            /**
-             * The image id for the instance. See EC2 console for the list of instances available to you.
-             */
             public static final String IMAGE_ID = "ec2.image.id";
-
-            /**
-             * The <a href="http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ec2-key-pairs.html">EC2 key pair</a> name for
-             * this instance. If you want to connect manually to the instance, this will be used to access it (unless overriden
-             * with {@link org.wildfly.extras.sunstone.api.impl.Config.Node.EC2#SSH_PRIVATE_KEY} or
-             * {@link org.wildfly.extras.sunstone.api.impl.Config.Node.EC2#SSH_PRIVATE_KEY_FILE}.
-             *
-             * <p>
-             * To see the key pairs available to you, head to the EC2 management console.
-             * </p>
-             */
             public static final String KEY_PAIR = "ec2.keyPair";
-            /**
-             * The identifier for the security group for this instance. See EC2 management console for list of your security groups.
-             */
             public static final String SECURITY_GROUPS = "ec2.securityGroups";
             public static final String SECURITY_GROUP_IDS = "ec2.securityGroupIds";
-
-            /**
-             * The list of ports to be available to the outer world.
-             */
             public static final String INBOUND_PORTS = "ec2.inboundPorts";
-
-            /**
-             * The ssh user for connecting to the instance. Overrides default setting (usually "root") but is currently unused.
-             * Use {@link org.wildfly.extras.sunstone.api.impl.Config.Node.EC2#SSH_PRIVATE_KEY_FILE} instead.
-             */
             public static final String SSH_USER = "ec2.ssh.user";
-            /**
-             * The ssh user's password for connecting to the instance. Overrides default setting but is currently unused.
-             * Use {@link org.wildfly.extras.sunstone.api.impl.Config.Node.EC2#SSH_PRIVATE_KEY_FILE} instead.
-             */
             private static final String SSH_PASSWORD = "ec2.ssh.password";
-            /**
-             * The ssh private key (not the file of the key!). In case you want to pass a private key manually.
-             * Currently unused in favor of {@link org.wildfly.extras.sunstone.api.impl.Config.Node.EC2#SSH_PRIVATE_KEY_FILE}.
-             */
             private static final String SSH_PRIVATE_KEY = "ec2.ssh.privateKey";
-            /**
-             * The path to the private key which should be used to connect to the instance. Necessary for establishing an ssh channel for your tests.
-             */
             public static final String SSH_PRIVATE_KEY_FILE = "ec2.ssh.privateKeyFile";
-
-            /**
-             * How long to wait for ports to open after the instance is started.
-             */
             public static final String WAIT_FOR_PORTS_TIMEOUT_SEC = "ec2.waitForPorts.timeoutSec";
-            /**
-             * What ports to wait for. Port 22 is recommended, since you'll likely want to run some commands through ssh on your machine.
-             */
             public static final String WAIT_FOR_PORTS = "ec2.waitForPorts";
-            /**
-             * Unencoded Value of <a href="http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/user-data.html">EC2 User data</a>.
-             * This property takes precedence over the {@value #BOOT_SCRIPT_FILE}.
-             *
-             * @see #BOOT_SCRIPT_FILE
-             */
             public static final String USER_DATA = "ec2.userData";
-            /**
-             * Path to a file with unencoded value of
-             * <a href="http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/user-data.html">EC2 User data</a>. This property is
-             * only used when {@value #BOOT_SCRIPT} property is not specified.
-             *
-             * @see #BOOT_SCRIPT
-             */
             public static final String USER_DATA_FILE = "ec2.userData.file";
             public static final String SUBNET_ID = "ec2.subnetId";
         }
