db4f02969f7779787ef0065d73ee82d0cac619b5
Ivan Straka <is.straka@gmail.com>
2024-01-23 15:40:09 +0100

    [#215] Migrate from pgsql standalone server to pgsql flexible server

--- azure/src/main/java/sunstone/azure/impl/AzureIdentifiableSunstoneResourceUtils.java
+++ azure/src/main/java/sunstone/azure/impl/AzureIdentifiableSunstoneResourceUtils.java
@@ -3,7 +3,7 @@ package sunstone.azure.impl;
 
 import com.azure.resourcemanager.appservice.models.WebApp;
 import com.azure.resourcemanager.compute.models.VirtualMachine;
-import com.azure.resourcemanager.postgresql.models.Server;
+import com.azure.resourcemanager.postgresqlflexibleserver.models.Server;
 import sunstone.inject.Hostname;
 import sunstone.core.exceptions.SunstoneException;
 import sunstone.core.exceptions.UnsupportedSunstoneOperationException;
